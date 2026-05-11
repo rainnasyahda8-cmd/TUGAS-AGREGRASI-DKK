9044f21e2d1c4c1dd13ee2e9f902ca59598703fc
rainnasyahda8 <rainnasyahda8@gmail.com>
2026-05-11 15:12:21 +0700

    menambahkan delete

--- agregasi.cpp
+++ agregasi.cpp
@@ -4,4 +4,23 @@ using namespace std;
 #include "anak.h"
 #include "ibu.h"
 
-int main()
+int main() {
+    ibu* varIbu = new ibu("dini");
+    ibu* varIbu = new ibu("novi");
+    ibu* varAnak1 = new anak("tono");
+    ibu* varAnak2 = new anak("rini");
+    ibu* varanak3 = new anak("dewi");
+
+    varIbu->tambahkanAnak(varAnak1);
+    varIbu->tambahkanAnak(varAnak2);
+    varIbu2->tambahkanAnak(varAnak3);
+    varIbu2->tambahkanAnak(varAnak1);
+
+    varIbu->cetakAnak();
+    varIbu2->cetakANak();
+
+    delete varIbu;
+    delete varIbu2;
+    delete varAnak1;
+    
+}
