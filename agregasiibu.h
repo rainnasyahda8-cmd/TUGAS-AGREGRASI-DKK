db9192fb3f9ecc10ce64557ea3ca1963d0bba0e9
rainnasyahda8 <rainnasyahda8@gmail.com>
2026-05-11 15:06:35 +0700

    menambahkan include

--- agregasiibu.h
+++ agregasiibu.h
@@ -5,9 +5,9 @@
 class ibu {
 public:
     string nama;
-    vector<anak*> daftat_anak;
+    vector <anak*> daftar_anak;
 
-    ibu(string pNama) :nama(pNAma {
+    ibu(string pNama) :nama(pNAma) {
         cout << "ibu \"" << nama << "\" ada\n";
     }
     ~ibu(){
@@ -15,4 +15,18 @@ public:
     }
     void tambahAnak(anak*);
     void cetakAnak();
+};
+void ibu::tambahAnak(anak* pAnak){
+    daftar_anak.push_back(pAnak);
 }
+void ibu::cetakAnak(){
+    cout << "Daftar Anak dari Ibu \"" << this->nama << "\":\n";
+    /*for (auto& a : daftar_anak) {
+        cout << a->nama << "\n";
+    }*/
+   for (int i = 0; i < daftar_anak.size(); i++){
+        cout << daftar_anak[i]->nama << endl;
+   }
+   cout << endl;
+}
+#endif
